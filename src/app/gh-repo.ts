c431923059ef99a186897980c8e939d5b73bcaab
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-24 16:39:19 +0300

    Create both user and repository model classes

---
+++ src/app/gh-repo.ts
@@ -0,0 +1,6 @@
+export class GhRepo {
+  public name: string;
+  public description: string;
+  public html_url: string;
+  public created_at: string;
+}
---
+++ src/app/gh-user.ts
@@ -0,0 +1,9 @@
+export class GhUser {
+  public login: string;
+  public avatar_url: string;
+  public html_url: string;
+  public name: string;
+  public public_repos: number;
+  public followers: number;
+  public following: number;
+}
