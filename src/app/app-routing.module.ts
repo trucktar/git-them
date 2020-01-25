e6bed92363f41653bb8c1f2ea9877a4d5b5413be
Nyota Mwangi <nyota.benjamin@gmail.com>
2020-01-25 12:10:39 +0300

    Configure the routing module and paths to different views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { UserSearchComponent } from "./user-search/user-search.component";
+import { UserReposComponent } from "./user-repos/user-repos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/user-repos/trucktar", pathMatch: "full" },
+  { path: "user-search", component: UserSearchComponent },
+  { path: "user-repos/:username", component: UserReposComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
